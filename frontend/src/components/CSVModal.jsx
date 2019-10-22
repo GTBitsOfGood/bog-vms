80ff11b5f9a4bad6812976e12442ac412e774ba0
Sneha Kadiyala <skadiyala3@gatech.edu>
2019-10-21 22:33:33 -0400

    Tweaks to add route to backend
    Signed-off-by: Sneha Kadiyala <skadiyala3@gatech.edu>

--- frontend/src/components/CSVModal.jsx
+++ frontend/src/components/CSVModal.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import { Button, Form, FormGroup, Label, Input, FormText, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 class CSVModal extends React.Component {
@@ -20,22 +19,21 @@ class CSVModal extends React.Component {
       
     }));
   }
-
-
+  
   submit(event) {
     const file = event.target.files[0];
     var reader = new FileReader();
-
+  
     reader.onload = function(e) {
 
       this.setState({file: reader.result})
+      const header_end = reader.result.indexOf('\n')
+      const header = reader.result.substring(0, header_end)
       console.log(this.state)
 
     }.bind(this);
 
-
-    reader.readAsText(file)
-    
+    reader.readAsText(file)   
   }
 
   render() {
@@ -45,17 +43,19 @@ class CSVModal extends React.Component {
         <Modal isOpen={this.state.modal} toggle={this.toggle}>
           <ModalHeader toggle={this.toggle}>Upload CSV Files Here</ModalHeader>
           <ModalBody>
-            <Form>
+            <Form action="/csvAddition" method="POST">
               <FormGroup>
                 <Label for="exampleFile">File</Label>
-                <Input type="file" name="file" id="exampleFile" onChange={this.submit}/>
+                <Input type="file" name="file" id="inputFile" onChange={this.submit}/>
                 <FormText color="muted">
                   Upload your CSV file with the information about your volunteers!
                   Please format your header with the following column names: 
                   [first_name, last_name, phone_number, email, dob, address]
                 </FormText>
+                <a href='https://docs.google.com/spreadsheets/d/1a2f5jOrtaTU9KYkuCtRQTsjCGe5_rCk7c24_F0f2BUw/export?format=xlsx' download>CSV Header Template</a>
+                <Button color="primary" >Submit</Button>
               </FormGroup>
-            </Form >
+            </Form>
           </ModalBody>
           <ModalFooter>
             <Button color="primary" onClick={this.toggle}>Submit</Button>
