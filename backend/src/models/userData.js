a7b6ccef068a49094486d44bf52cf64989caab57
Sneha Kadiyala <skadiyala3@gatech.edu>
2019-11-05 19:51:29 -0500

    fixed merge conflicts

--- backend/src/models/userData.js
+++ backend/src/models/userData.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+// define schema for user collection (user model)
 const userDataSchema = new mongoose.Schema(
   {
     role: {
@@ -10,14 +11,7 @@ const userDataSchema = new mongoose.Schema(
     status: {
       type: String,
       default: 'new',
-      enum: [
-        'accepted',
-        'denied',
-        'no_response',
-        'got_initial_email',
-        'has_volunteered',
-        'new'
-      ]
+      enum: ['accepted', 'denied', 'has_volunteered', 'new']
     },
     bio: {
       first_name: { type: String },
@@ -30,53 +24,19 @@ const userDataSchema = new mongoose.Schema(
       state: { type: String },
       zip_code: { type: String }
     },
-    history: {
-      volunteer_interest_cause: { type: String },
-      volunteer_support: { type: String },
-      volunteer_commitment: { type: String },
-      previous_volunteer_experience: { type: String }
-    },
-    availability: {
-      weekday_mornings: { type: Boolean, default: false },
-      weekday_afternoons: { type: Boolean, default: false },
-      weekday_evenings: { type: Boolean, default: false },
-      weekend_mornings: { type: Boolean, default: false },
-      weekend_afternoons: { type: Boolean, default: false },
-      weekend_evenings: { type: Boolean, default: false }
-    },
+    history: { type: String },
     skills_interests: {
-      admin_office: { type: Boolean, default: false },
-      admin_virtual: { type: Boolean, default: false },
-      atlanta_shelter: { type: Boolean, default: false },
-      orlando_shelter: { type: Boolean, default: false },
-      graphic_web_design: { type: Boolean, default: false },
-      special_events: { type: Boolean, default: false },
-      grant_writing: { type: Boolean, default: false },
-      writing_editing: { type: Boolean, default: false },
-      social_media: { type: Boolean, default: false },
-      fundraising: { type: Boolean, default: false },
-      finance: { type: Boolean, default: false },
-      office_maintenance_housekeeping: { type: Boolean, default: false },
-      international_projects: { type: Boolean, default: false },
-      volunteer_coordination: { type: Boolean, default: false },
-      outreach: { type: Boolean, default: false },
-      languages: { type: String, default: '' },
-      skills_qualifications: { type: String }
-    },
-    referral: {
-      friend: { type: Boolean, default: false },
-      newsletter: { type: Boolean, default: false },
-      event: { type: Boolean, default: false },
-      volunteer_match: { type: Boolean, default: false },
-      internet: { type: Boolean, default: false },
-      social_media: { type: Boolean, default: false }
+      type: String
+      //list[] skills
     },
     employment: {
       industry: { type: String },
       occupation: { type: [String] }
     }
   },
-  { timestamps: true }
+  {
+    timestamps: true
+  }
 );
 
 userDataSchema.virtual('name').get(function() {
