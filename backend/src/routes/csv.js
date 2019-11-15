b326415e669c836f421627c9899632240ceaba73
Sneha Kadiyala <skadiyala3@gatech.edu>
2019-11-15 15:43:53 -0500

    blahbalhbalbah

--- backend/src/routes/csv.js
+++ backend/src/routes/csv.js
@@ -3,68 +3,53 @@ const express = require('express');
 const router = express.Router();
 const UserData = require('../models/userData');
 const fs = require('fs');
-const multer  = require('multer')
+const multer = require('multer');
 
-const upload = multer({ dest: 'uploads/' })
+const upload = multer({ dest: 'uploads/' });
 
-router.get("/CSVerror", function(req, res, next) {
+router.get('/CSVerror', function(req, res, next) {
   res.type('txt').send('Not found');
-
-})
+});
 router.post('/csvAddition', upload.single('input'), function(req, res, next) {
   const file = req.file;
-  
+
   fs.readFile(file.path, 'utf-8', (err, data) => {
-    
     var allTextLines = data.split(/\r\n|\n/);
     var headers = allTextLines[0].split(',');
     var lines = [];
 
-    //Upload csv data into MongoDB
-    var err = false;
-    for (var i=1; i<allTextLines.length; i++) {
-        var data = allTextLines[i].split(',');
-        if (data.length == headers.length) {
+    for (var i = 1; i < allTextLines.length; i++) {
+      var data = allTextLines[i].split(',');
+      if (data.length == headers.length) {
+        const date = new Date(data[4]);
 
-          const date = new Date(data[4]);
-
-          if(Object.prototype.toString.call(date) === "[object Date]") {
-
-            if (!isNaN(date.getTime())) {
+        if (Object.prototype.toString.call(date) === '[object Date]') {
+          if (!isNaN(date.getTime())) {
             const newUser = new UserData({
               bio: {
-                first_name: data[0], 
-                last_name: data[1], 
-                phone_number: data[2], 
-                email: data[3], 
-                date_of_birth: date, 
-                street_address: data[5], 
+                first_name: data[0],
+                last_name: data[1],
+                phone_number: data[2],
+                email: data[3],
+                date_of_birth: date,
+                street_address: data[5],
                 city: data[6],
                 state: data[7],
                 zip_code: data[8]
-              }});
+              }
+            });
 
             newUser.save().catch(console.log);
-            console.log("Added " + data[0] + " " + data[1]);
-
-            } else {
-              err = true;
-            }
-          }  else {
+          } else {
             err = true;
           }
+        } else {
+          err = true;
         }
+      }
     }
-    if(err) {
-      res.redirect('/CSVerror');
-    } else {
-      res.redirect('/');
-    }
-    
-
+    res.redirect('/');
   });
-
-
 });
 
 module.exports = router;
