7d160c9694e50e3ee011ebb5002f4253777b4ff5
Dheeraj Bandaru <BandaruDheeraj@users.noreply.github.com>
2020-02-04 19:11:09 -0500

    event card and card style

---
+++ frontend/src/components/AdminDash/events/EventCard.jsx
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import * as Table from '../shared/tableStyles';
+import Loading from 'components/Shared/Loading';
+import { Icon } from 'components/Shared';
+import styled from 'styled-components';
+import { Button } from 'reactstrap';
