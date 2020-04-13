e4436365ca73b7fa6605ce727efa78891e5be412
Jake Deerin <2016deerinjo@gmail.com>
2020-04-12 21:58:56 -0400

    Added basic TimeSlotMenu component (currently still missing important functionality/styles)

---
+++ frontend/src/components/Shared/TimeSlotMenu.jsx
@@ -0,0 +1,95 @@
+/**
+ * A menu that allows users to select and submit times they're signing up for in
+ * select intervals
+ */
+
+import React from 'react';
+import { Button } from 'reactstrap';
+import { Icon } from 'components/Shared';
+import moment from 'moment';
+import styled from 'styled-components';
+
+// TODO: Replace colors w/ theme colors (i.e. not hardcoded values)
+const Styled = {
+    TimeSlotsContainer: styled.div `
+        padding: 1em 0.5em;
+        background: white;
+        color: black;
+        border: solid 1px #c4c4c4;
+        border-radius: 2px;
+    `,
+
+    TimeSlot: styled.div `
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 0.5em;
+        min-width: 12em;
+    `
+};
+
+/**
+ * Given a JS Date object, copies it and returns a Moment.js copy of it rounded
+ * UP to the nearest 30 minute interval (e.g. 4:17 becomes 4:00)
+ */
+function roundTimeUpToHalfHour(dateTime) {
+    const momentTime = moment(dateTime);
+    const minutes = momentTime.minutes();
+    // Assuming minutes < 60, rounds down to either 0 or 30
+    const roundedMinutes = Math.floor(minutes / 30) * 30;
+
+    const roundedTime = momentTime.startOf('hour').add(roundedMinutes, 'minutes');
+    return roundedTime;
+}
+
+// TODO: Implement changes to signed-up times when they're selected
+// TODO: Implement done (i.e. submission) functionality
+// TODO: Implement undo functionality
+// TODO: Only show undo if there are changes made
+
+
+// NOTE: Signed up times should contain the STARTING time of each integer we're
+// already signed up for
+function TimeSlotMenu({startDateTime, endDateTime, selectedTimes}) {
+    // Copy sign-up times so we can revert/undo changes to them if necessary
+    const oldSelectedTimes = (selectedTimes) ? [...selectedTimes] : [];
+    const roundedStartTime = roundTimeUpToHalfHour(startDateTime);
+
+    console.log(roundedStartTime);
+    console.log(endDateTime);
+
+    // Generate times for timeslot intervals
+    const slotTimes = [];
+    for (let currentTime = roundedStartTime; currentTime.isBefore(endDateTime); currentTime.add(30, 'minutes')) {
+        slotTimes.push(currentTime.clone());
+    }
+    // Missing last interval, so add it manually
+    // TODO: Avoid this if possible?
+    slotTimes.push(slotTimes[slotTimes.length - 1].clone().add(30, 'minutes'));
+
+    const timeslotElements = [];
+    for (let i = 0; i < slotTimes.length - 1; i++) {
+        // TODO: Actually handle input to/from checkboxes
+        const startTime = slotTimes[i];
+        const endTime = slotTimes[i+1];
+        // TODO: Pre-check selected times
+        const timeslotText = `${startTime.format('LT')} - ${endTime.format('LT')}`;
+        const timeslotName = `${startTime.format('LT')}-${endTime.format('LT')}`;
+        console.log(`${timeslotText}`);
+        timeslotElements.push (
+            <Styled.TimeSlot key={i}>
+                <input type="checkbox" name={timeslotName}/>
+                <label for={timeslotName}>{timeslotText}</label>
+            </Styled.TimeSlot>
+        );
+    }
+
+    return (
+        <Styled.TimeSlotsContainer>
+            {timeslotElements}
+        </Styled.TimeSlotsContainer>
+    );
+}
+
+
+export default TimeSlotMenu;
